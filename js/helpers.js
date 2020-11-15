7011e2bc0f604bc5714fc25b35542a3ffde4c360
Seeam2590 <seeamshahid@gmail.com>
2020-11-14 22:39:25 -0500

    added a carousel to tell a story

---
+++ js/helpers.js
@@ -0,0 +1,16 @@
+/* * * * * * * * * * * * * *
+*         Carousel         *
+* * * * * * * * * * * * * */
+
+// define carousel behaviour
+let carousel = $('#stateCarousel');
+
+// prevent rotating
+carousel.carousel({
+    interval: false
+})
+
+// on button click switch view
+function switchView(){
+    carousel.carousel('next')
+}
