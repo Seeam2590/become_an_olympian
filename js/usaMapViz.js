53efaa29f727b0420523ea70157cb34b3fc127fa
Seeam2590 <seeamshahid@gmail.com>
2020-11-15 22:04:17 -0500

    map tooltip showing up

--- js/usaMapViz.js
+++ js/usaMapViz.js
@@ -39,9 +39,14 @@ class UsaMapViz {
         vis.projection = d3.geoNaturalEarth()
             .scale(vis.width / 1.3 / Math.PI)
             .translate([vis.width / 2, vis.height / 2])
+
+        // append tooltip
+        vis.tooltip = d3.select("#" + vis.parentElement).append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+
         vis.wrangleData();
     }
-
 
     /*
      *  Data wrangling
@@ -54,32 +59,74 @@ class UsaMapViz {
         });
 
         vis.maxAthletes = vis.data.reduce((a,b)=>a.athletes>b.athletes?a:b).athletes;
+
+        // Color scale
+        vis.color = d3.scaleSequential(d3.interpolateGreens)
+            .domain([0, vis.maxAthletes]);
+
+        vis.worldGeo.features.forEach(function (d) {
+            var info = vis.data.filter(e => e.code == d.id);
+            if(info.length > 0) {
+                d.color = vis.color(info[0].athletes)
+                d.athletes = info[0].athletes
+                d.country = info[0].country
+            } else {
+                d.color = "#fff";
+                d.athletes = 0;
+                d.country = d.id
+            }
+        });
+
         vis.updateVis();
     }
 
     updateVis() {
         let vis = this;
 
-        // Color scale
-        vis.color = d3.scaleSequential(d3.interpolateGreens)
-            .domain([0, vis.maxAthletes])
-
         // Draw the map
         vis.svg.append("g")
             .selectAll("path")
             .data(vis.worldGeo.features)
             .enter().append("path")
-            .attr("fill", d => {
-                var info = vis.data.filter(e => e.code == d.id);
-                if(info.length > 0)
-                    return vis.color(info[0].athletes)
-                else
-                    return "#fff";
+            .on("mouseover", function(event, d){
+                vis.tipMouseover(event, d);
             })
+            .on("mouseout", function(){
+                vis.tipMouseout();
+            })
+            .attr("fill", d => { return d.color})
             .attr("d", d3.geoPath()
                 .projection(vis.projection)
             )
             .style("stroke", "#000")
     }
+
+    // tooltip mouseover event handler
+    tipMouseover(event, d) {
+        // To make the tooltip easier to read
+        let vis = this;
+        vis.tooltip.html(`
+         <div style="border: thin solid grey; border-radius: 5px; background: white; width: 200px">
+             <h5>${d.country}</h5>
+             <h6>Olympic Athletes: ${d.athletes}</h6>
+         </div>`
+        )
+            .style("left", event.pageX - 200 + "px")
+            .style("top", 100 + "px")
+            .style("text-align", "center")
+            .transition()
+            .duration(300) // ms
+            .style("opacity", 0.9)
+
+    };
+
+    // tooltip mouseout event handler
+    tipMouseout() {
+        let vis = this;
+
+        vis.tooltip.transition()
+            .duration(300) // ms
+            .style("opacity", 0); // don't care about position!
+    };
 }
 
