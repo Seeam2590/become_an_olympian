c360841614bade811d6af3f0130ec76ff75c4158
Seeam2590 <seeamshahid@gmail.com>
2020-11-16 19:11:31 -0500

    scatter plot showing up

--- js/usaScatterViz.js
+++ js/usaScatterViz.js
@@ -34,6 +34,42 @@ class UsaScatterViz {
             .append("g")
             .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
 
+        // Add X axis
+        vis.x = d3.scaleLinear()
+            .domain([0, 18000])
+            .range([ 0, vis.width ]);
+
+        vis.svg.append("g")
+            .attr("transform", "translate(0," + vis.height + ")")
+            .call(d3.axisBottom(vis.x));
+
+        // Add Y axis
+        vis.y = d3.scaleLinear()
+            .domain([0, 6000])
+            .range([ vis.height, 0]);
+
+        vis.svg.append("g")
+            .call(d3.axisLeft(vis.y));
+
+        // Add a scale for bubble size
+        //vis.z = d3.scaleLinear()
+          //  .domain([200000, 1310000000])
+           // .range([ 1, 40]);
+
+        // Add dots
+        vis.svg.append('g')
+            .selectAll("dot")
+            .data(vis.data)
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) { return vis.x(d.athletes); } )
+            .attr("cy", function (d) { return vis.y(d.total); } )
+            .attr("r", 6)
+            //.attr("r", function (d) { return z(d.pop); } )
+            .style("fill", "#69b3a2")
+            .style("opacity", "0.7")
+            .attr("stroke", "black")
+
         vis.wrangleData();
     }
 
